c8e1387a71e5acdab94e144cd51e09a9b2994696
jgomez <jgomez@iesromerovargas.com>
2023-04-20 21:18:19 +0200

    test 5 action

--- app.py
+++ app.py
@@ -1,25 +1,17 @@
-
 # From https://github.com/datademofun/heroku-basic-flask
-# Nota: en ese tutorial se ve como hacerlo 
-#  - con Heroku CLI
-#  - sin usar GitHub
+# Nota: en ese tutorial se ve como hacerlo
+# - con Heroku CLI
+# - sin usar GitHub
 #   Aunque luego al final lo manda a GitHub
 
 from flask import Flask, render_template
 from datetime import datetime
-
-
 import locale
 
 
-#####testing 4
-
-
-
-
 app = Flask(__name__)
-      #test
 locale.setlocale(locale.LC_TIME, '')
+
 
 @app.route('/')
 def homepage():
@@ -27,13 +19,14 @@ def homepage():
     the_time = datetime.now().strftime("%A, %d %b %Y %H:%M")
 
     # Render HTML with variable
-    return render_template("index.html", the_time=the_time, tema="dogs" )
+    return render_template("index.html", the_time=the_time, tema="dogs")
 
     # return f"""
-       # <h1>Hello heroku</h1>
+    # <h1>Hello heroku</h1>
     # <p>It is currently {the_time}.</p>
     # <img src="http://loremflickr.com/600/400" />
     # """
 
+
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True) 
+    app.run(debug=True, use_reloader=True)
